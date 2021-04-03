f2fd40391455bc9f690eb968d6cb694d595a84e4
briantylerec <brianmoraa@gmail.com>
2021-04-02 21:24:47 -0500

    cambios finales

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB0jJgBGOpEAfoi4lR9nEdJ8DsulZiH600",
+    authDomain: "stroop-games.firebaseapp.com",
+    projectId: "stroop-games",
+    storageBucket: "stroop-games.appspot.com",
+    messagingSenderId: "304677985045",
+    appId: "1:304677985045:web:febc13a922c86097dfe71c"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyB0jJgBGOpEAfoi4lR9nEdJ8DsulZiH600",
     authDomain: "stroop-games.firebaseapp.com",
